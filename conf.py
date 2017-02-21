ae0594f251801df33e78a2d95179712e62014b6d
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2017-02-21 13:54:24 +0100

    Test article

--- conf.py
+++ conf.py
@@ -204,13 +204,13 @@ TIMEZONE = "Europe/Paris"
 #
 
 POSTS = (
-    ("posts/*.md", "posts", "post.tmpl"),
     ("posts/*.rst", "posts", "post.tmpl"),
+    ("posts/*.md", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
 )
 PAGES = (
-    ("stories/*.md", "stories", "story.tmpl"),
     ("stories/*.rst", "stories", "story.tmpl"),
+    ("stories/*.md", "stories", "story.tmpl"),
     ("stories/*.txt", "stories", "story.tmpl"),
 )
 
