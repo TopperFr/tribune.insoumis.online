88ceca76e281589e622ff348bc2ced248746c8d9
Lucas Gautheron <lucas.gautheron@gmail.com>
2017-02-22 03:18:16 +0100

    Author change

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "France Insoumise"  # (translatable)
+BLOG_AUTHOR = "Discord Insoumis"  # (translatable)
 BLOG_TITLE = "Tribune de la France Insoumise"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
